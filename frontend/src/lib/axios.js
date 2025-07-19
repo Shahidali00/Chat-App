45b1f82bb4667fc2ba09b9f69c255fa0cbec1892
Shahid <sunasarashahid23@gmail.com>
2025-07-19 14:22:14 +0530

    new changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://chat-app-bws2.onrender.com/api',
     withCredentials: true
 })
