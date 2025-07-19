cbb5f87354745ee772904420b780b508cb507eae
Shahid <sunasarashahid23@gmail.com>
2025-07-19 15:26:20 +0530

    new chjanges

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://chat-app-bws2.onrender.com/api',
+    baseURL:import.meta.env.MODE === 'development' ?  'http://localhost:3000' : import.meta.env.REACT_APP_API_URL,
     withCredentials: true
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { persist } from 'zustand/middleware'
 import { axiosInstance } from '../lib/axios'
 import { toast } from 'react-hot-toast'
 import { io } from "socket.io-client";
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000";
+const BASE_URL = import.meta.env.MODE === 'development' ?  'http://localhost:3000' : import.meta.env.REACT_APP_API_URL;
 
 
 export const useAuthStore = create((set, get) => ({
