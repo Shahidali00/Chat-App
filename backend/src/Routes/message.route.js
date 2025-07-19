78041f305954f348d2c4e822232d2f1c544f2cf5
Shahid <sunasarashahid23@gmail.com>
2025-07-19 17:32:01 +0530

    new make files

--- backend/src/Routes/message.route.js
+++ backend/src/Routes/message.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { protectRoute } from "../middlewaress/auth.middlewares.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
