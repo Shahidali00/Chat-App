e1976a27b4f859fbee520202704d952e2e789804
Shahid <sunasarashahid23@gmail.com>
2025-07-23 14:58:39 +0530

    some changes

--- backend/src/Routes/auth.route.js
+++ backend/src/Routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { checkAuth, login, logout, signup } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middlewares/auth.middleware';
+import { protectRoute } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import authRoute from "./Routes/auth.route.js";
-import messageRoutes from "./Routes/message.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import path from "path";
-import  connectDB  from "./lib/db.js";
-
+import connectDB from "./lib/db.js";
+import authRoute from "./Routes/auth.route.js";
+import messageRoutes from "./Routes/auth.route.js";
 
 import { app, server } from "./lib/socket.js";
 
